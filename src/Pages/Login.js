62b282de1ea5d4ba6145c09f8cf6ffcda21ead55
Guy-Gustave <guymoustapha@gmail.com>
2022-05-13 12:03:53 +0200

    style login page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import {useState} from 'react';
+import { useState } from 'react';
+import '../styles/login.css'
 
 const Login = () => {
 
@@ -18,25 +19,36 @@ const Login = () => {
                 email,
                 password
             });
-           // console.log(response.data.access_token, response.data.data);
+            // console.log(response.data.access_token, response.data.data);
             localStorage.setItem('token', response.data.access_token);
             localStorage.setItem('user', response.data.data);
             setIsLoading(false);
-           window.location.href = '/admin';
+            window.location.href = '/admin';
         } catch (error) {
             setError(error.response.data.message);
             setIsLoading(false);
         }
     }
-    return ( <div>
+    return (<div className='login'>
         <h1>Login</h1>
-        <form action="" onSubmit={handleSubmit}>
-            <label htmlFor="">Email</label>
-            <input type="text" name="email" value={email} placeholder="Email" onChange={(e) => setEmail(() => e.target.value)}/>
-            <label htmlFor="">Password</label>
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(() => e.target.value)}/>
-            <button type="submit">Login</button>
-        </form>
+        <div className='log-infos'>
+            <form action="" onSubmit={handleSubmit}>
+                <div class="email">
+
+                    <label htmlFor="">Email</label>
+                    <input type="text" name="email" value={email} placeholder="Email" onChange={(e) => setEmail(() => e.target.value)} />
+                </div>
+                <div class="password">
+
+                    <label htmlFor="">Password</label>
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(() => e.target.value)} />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            <div class="right-login">
+
+            </div>
+        </div>
 
         {isLoading && (
             <div>
@@ -48,7 +60,7 @@ const Login = () => {
                 {error}
             </div>
         )}
-    </div> );
+    </div>);
 }
- 
+
 export default Login;
