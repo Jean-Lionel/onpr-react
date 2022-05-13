6164776fde36b501d281b3ed95a7c48f199136ae
Guy-Gustave <guymoustapha@gmail.com>
2022-05-13 12:28:18 +0200

    add media queries

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -51,8 +51,8 @@ const Login = () => {
         </div>
 
         {isLoading && (
-            <div>
-                Is Loading...
+            <div className='loding'>
+                <h3>Is Loading...</h3>
             </div>
         )}
         {error && (
