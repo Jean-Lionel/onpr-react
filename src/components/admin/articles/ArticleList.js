769c5831ba6024337f9a0edf6da2a00c3a2b8eda
Guy-Gustave <guymoustapha@gmail.com>
2022-05-16 19:40:09 +0200

    display responsive grid using material

--- src/components/admin/articles/ArticleList.js
+++ src/components/admin/articles/ArticleList.js
@@ -2,10 +2,23 @@ import useFetchData from "../../../utility/useFecthData";
 import { Link } from "react-router-dom"
 import DataTable from "../../../Test/DateTabeleTest";
 import '../../../styles/web.css'
+import { Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 const ArticleList = () => {
     let { data: articles, isLoading, error } = useFetchData("articles");
     let articlesList = articles?.data?.data
+
     return (
         <div>
 
@@ -29,32 +42,24 @@ const ArticleList = () => {
                     {error}
                 </div>
             )}
-            <ul>
-
-                {articlesList && articlesList.map((article, index) => (
-                    <li key={index} className="article-item">
-                        <div>
-                            <dl>
-                                <dt>Title</dt>
-                                <dd>{article.title}</dd>
-                            </dl>
-                            <dl>
-                                <dt>Détail</dt>
-                                <dd>
-                                    {article.body}
-                                </dd>
-                            </dl>
-                            <dl>
-                                <dt>Image</dt>
-                                <dd>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {/* {Array.from(Array(6)).map((_, index) => ( */}
+                    {articlesList && articlesList.map((article, index) => (
+                        <Grid item xs={2} sm={4} md={4} key={index}>
+                            <Item>
+                                <h3>{article.title}</h3>
+                                <h5>{article.body}</h5>
+                                <div class="image">
                                     {article.image}
-                                </dd>
-                            </dl>
+                                </div>
+                            </Item>
+                        </Grid>
+                    ))}
+                </Grid>
 
-                        </div>
-                    </li>
-                ))}
-            </ul>
+              
+            </Box>
 
         </div>
     );
