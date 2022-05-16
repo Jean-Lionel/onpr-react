3c72a32fdc426f7a7a90fdb18c089d00754cb8c5
Guy-Gustave <guymoustapha@gmail.com>
2022-05-16 20:38:10 +0200

    add carroussel and import in web portal

--- src/Pages/WebPortal.js
+++ src/Pages/WebPortal.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ArticleList from '../components/admin/articles/ArticleList'
 
 import '../styles/web.css'
+import Carroussel from './web/Carroussel'
 
 export default function WebPortal() {
   return (
     <div className="web">
       <div className='accueil'>
         <div className='infos'>
-
+          <Carroussel />
 
         </div>
         <div className='publicities'>
