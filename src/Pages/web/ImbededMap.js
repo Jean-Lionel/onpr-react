337e167538b670f4b1549de0ea6ede0ea11f66c6
Guy-Gustave <guymoustapha@gmail.com>
2022-05-18 22:37:28 +0200

    Add imbeded map localisation

--- src/Pages/web/ImbededMap.js
+++ src/Pages/web/ImbededMap.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function () {
+const  ImbededMap =() => {
   return (
     <div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1991.4221025787238!2d29.378463658151475!3d-3.3881989993834134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc75bfb80cd8c29a2!2zM8KwMjMnMTcuNSJTIDI5wrAyMic0Ni40IkU!5e0!3m2!1sfr!2sbi!4v1652729271396!5m2!1sfr!2sbi" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1991.4221025787238!2d29.378463658151475!3d-3.3881989993834134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc75bfb80cd8c29a2!2zM8KwMjMnMTcuNSJTIDI5wrAyMic0Ni40IkU!5e0!3m2!1sfr!2sbi!4v1652729271396!5m2!1sfr!2sbi" width="600" height="200">
+       </iframe>
+
     </div>
   )
 }
+
+export default ImbededMap 
