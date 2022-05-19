8191590ed889266a96db8f2aff366c8210768ad0
Jean-Lionel <nijeanlionel@gmail.com>
2022-05-19 04:32:06 +0200

    implementing search bar componet

--- src/Pages/Admin/contisation/AddCotisationAfilier.jsx
+++ src/Pages/Admin/contisation/AddCotisationAfilier.jsx
@@ -1,17 +1,31 @@
 import { Box , Input, Select, MenuItem,Alert} from "@mui/material";
 import useReadExcel from "../../../utility/useReadExcel";
-import {useState} from "react"
-import axios from "axios";
+import {useEffect, useState} from "react"
 import useFetchDataWithPagination from "../../../utility/useFetchDataWithPagination";
+import usePostDate from "../../../utility/usePostData";
+import { useHistory } from "react-router-dom";
 
 const AddCotisationAfilier = () => {
     const [instutionId, setInstutionId] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
     const {data : institutions } = useFetchDataWithPagination("/institutions/groupby/AFFILIERS");
+    const {response, isLoading, error, finished,submitData : saveData} = usePostDate();
     
     const loadInstitutions = institutions?.data ?? [];
+    const history = useHistory();
+
+    useEffect(() => {
+       
+        if(finished) {
+            history.reload();
+        }
+        return () => {
+            
+        };
+    }, [finished,history]);
 
     const {data,  dataTable, handleFileUpload}  = useReadExcel();
-    const [errorMessage, setErrorMessage] = useState("");
+   
     const submitData = (e) => {
         e.preventDefault();
         // console.log(data);
@@ -19,19 +33,19 @@ const AddCotisationAfilier = () => {
         postData.append('data', JSON.stringify(data));
         postData.append('institution_id', instutionId);  // name of the file
         
-        axios.post("cotisations", postData)
-        .then(res => {
-            console.log(res);
-        }).catch(err => {
-            console.log(err);
-            setErrorMessage(err.response.data.message);
-        })
+        saveData("cotisations", postData); 
+
+        // axios.post("cotisations", postData)
+        // .then(res => {
+        //     console.log(res);
+        // }).catch(err => {
+        //     console.log(err);
+        //     setErrorMessage(err.response.data.message);
+        // })
         
     }
     return ( 
         <Box>
-
-            
             {errorMessage && (
                 <Alert severity="error"> {errorMessage}</Alert>
             )}
@@ -65,8 +79,8 @@ const AddCotisationAfilier = () => {
                 ))}
             </Select>
             <Input  type="file"  label="Chargement du fichier excel"  accept="csv,xlsx,xls"   onChange={handleFileUpload}/>
-            <button type="submit" >Enregistrer</button>
-            </form>
+            <button type="submit" onClick={submitData}  >Enregistrer</button>
+        </form>
         </div>
         </Box>
         <div>
