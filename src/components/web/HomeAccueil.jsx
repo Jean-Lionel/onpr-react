532632a5094a675e40ce2ca6f7923ceea2b80ce2
Jean-Lionel <nijeanlionel@gmail.com>
2022-07-13 05:00:25 +0200

    home acceuil

---
+++ src/components/web/HomeAccueil.jsx
@@ -0,0 +1,28 @@
+import { Box } from "@mui/material";
+
+const HomeAccueil = () => {
+    return (<Box>
+        <h1> Connaître l’ONPR</h1>
+        <main>
+            <p>L’idée de créer un organisme chargé de gérer les Pensions et les Risques Professionnels des Fonctionnaires, des Magistrats et des Agents de l’Ordre Judiciaire date des années 1960. En 1965, lors de la parution de la Loi du 09 mars 1965 portant les Principes Généraux de la Fonction Publique, les autorités du pays avaient déjà préconisé l’étude d’un système contributif basé sur l’affiliation des magistrats et des fonctionnaires à l’INSS.
+            </p>
+
+            <p>
+            Durant les années 80, les autorités ont tenté de mettre en place le régime des pensions pour les Fonctionnaires et les Magistrats mais il a eu des difficultés d’application dues essentiellement à l’importance de la contribution que l’Etat devait verser à l’INSS pour couvrir les périodes antérieures à la date d’affiliation ainsi que l’appréhension des Fonctionnaires et Magistrats de perdre certains droits acquis.
+            </p>
+            <p>
+            En 1990, l’idée de la création de ce régime fut reprise par la réforme administrative initiée.
+            </p>
+            <p>
+            En 1993 la crise qu’a connue le Burundi n’a pas permis de donner suite immédiatement à l’étude qui avait été menée à cet effet. Le Gouvernement du Burundi, avec l’appui de la Banque Mondiale, dans le cadre du projet PAGE (Projet d’Appui à la Gestion Economique) , a entrepris les démarches en vue d’initier une étude de faisabilité pour la mise en place d’un organisme chargé de gérer les branches « pensions et risques professionnels » en faveur des Magistrats et Fonctionnaires. En dates du 7 et 8 avril 2009, le Consultant recruté à cet effet a restitué son rapport au Gouvernement du Burundi lors de l’atelier de restitution et de formation sur la mise en place de l’Office National des Pensions et Risques Professionnels pour les Fonctionnaires, les Magistrats et les Agents de l’Ordre Judiciaire.
+            </p>
+
+            <p>     
+            Le Comité de pilotage ainsi que les participants ont opté pour un démarrage immédiat de l’organisme. C’est cela qui a donné naissance de l’ONPR par le Décret n°100/52 du 31 mars 2010 portant Création, Organisation et Fonctionnement de l’ONPR.
+                </p>
+         </main>
+ 
+    </Box>);
+}
+ 
+export default HomeAccueil;
